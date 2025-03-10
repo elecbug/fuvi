8871ed3f1ca0011c6f8c8cd663eb59c2f1c6ef62
elecbug <deveb1479@gmail.com>
2025-03-10 12:36:15 +0900

    temp: make fuvi point

---
+++ FuVi/Function.cs
@@ -0,0 +1,17 @@
+﻿namespace FuVi
+{
+    public class Function
+    {
+        private Func<decimal, decimal> _func;
+
+        public Function(Func<decimal, decimal> func)
+        {
+            _func = func;
+        }
+
+        public FuviPoint[] Sampling(decimal start, decimal end, decimal interval)
+        {
+
+        }
+    }
+}
---
+++ FuVi/FuviPoint.cs
@@ -0,0 +1,19 @@
+﻿namespace FuVi
+{
+    public class FuviPoint
+    {
+        public decimal X { get; set; }
+        public decimal Y { get; set; }
+        
+        public FuviPoint()
+        {
+            X = 0;
+            Y = 0;
+        }
+        public FuviPoint(decimal x, decimal y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
